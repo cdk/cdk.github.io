3277d74ed6ba2c00d6fe0dc3dee954ae361fb84a
John Mayfield <john@nextmovesoftware.com>
2022-04-06 20:56:03 +0100

    Left overs - from previous commit. This file was renamed.

--- src/js/imports.js
+++ src/js/imports.js
@@ -1,4 +1,4 @@
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import "../scss/style.scss";
+import "../css/style.css";
